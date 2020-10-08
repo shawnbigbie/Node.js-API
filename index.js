5b4f8e64e680acbdd6c8e4b6fab1be523d0df514
Shawn Bigbie <boboargos@outlook.com>
2020-10-08 17:47:11 -0600

    Upload

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import bodyParser from "body-parser";
 import usersRoutes from './routes/users.js';
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 
